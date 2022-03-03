bbc36283a9990f91a83401605f1e948fa7d70429
Sam Dennon <sameureka@me.com>
2022-03-03 15:34:53 -0800

    got the worker nodes going! :+1:

--- balenaRanchHand/hand/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-#docker run --d --privileged -v /var/run/docker.sock:/var/run/docker.sock \
-#iptables -F
-#update-alternatives --set iptables /usr/sbin/iptables-legacy
-#update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-
-curl -sfL https://get.k3s.io | K3S_URL=${K3S_URL} K3S_TOKEN=${K3S_TOKEN} sh -
