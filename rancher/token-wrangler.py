eea1f44f20e49ef8919f0cb2c83626b539d66b29
Sam Dennon <sameureka@me.com>
2022-03-17 06:21:42 -0700

    refactored functions, added try/except
    Tested with generated api key.
    
    Checked by @SamEureka 👍🏻

--- rancher/token-wrangler.py
+++ rancher/token-wrangler.py
@@ -1,32 +1,41 @@
 # balenaRancher/rancher/token-wrangler.py
 # Sam Dennon//2022
 
-from os import environ
+import os
 from time import sleep
-from os.path import exists, isfile 
+from os.path import exists, isfile
 import balena
 
-TOKEN_PATH = '/var/lib/rancher/k3s/server/node-token'
-BALENA_APP_ID = environ.get('BALENA_APP_ID')
-BALENA_API_KEY = environ.get('BALENA_API_KEY')
-sdk = balena.Balena()
+token_path = '/var/lib/rancher/k3s/server/node-token'
+var = 'K3S_TOKEN'
+app_id = os.environ.get('BALENA_APP_ID')
+token = os.environ.get('BALENA_API_KEY')
+api = balena.Balena()
+appVariables = api.models.environment_variables.application
 
-def addK3SToken(token):
-  sdk.auth.login_with_token(BALENA_API_KEY)
-  fleet_vars = sdk.models.environment_variables.application.get_all(BALENA_APP_ID)
-  list_from = [i['name'] for i in fleet_vars]
-  var_id = [i['id'] for i in fleet_vars if i['name'] == 'K3S_TOKEN']
-  if 'K3S_TOKEN' in list_from:
-    sdk.models.environment_variables.application.update(var_id[0], token)
-  else:
-    sdk.models.environment_variables.application.create(BALENA_APP_ID, 'K3S_TOKEN', token)
+def getAppVars(app_id, token):
+  try: api.auth.login_with_token(token)
+  except ValueError: print('Mistakes were made, check your API key.')
+  try: return appVariables.get_all(app_id)
+  except ValueError: print('Mistakes were made, unable to get fleet variable list')
+    
+def getVarId(app_id, token, var):
+  try: return [i['id'] for i in getAppVars(app_id, token) if i['name'] == var]
+  except ValueError: print('Mistakes were made, unable to do the thing...')
 
+def updateOrAddAppVar(app_id, token, var, var_val):
+  try: 
+    if var in [i['name'] for i in getAppVars(app_id, token)]:
+      appVariables.update(getVarId(app_id, token, var)[0], var_val)
+    else:
+      appVariables.create(app_id, var, var_val)
+  except ValueError: print('Nope, did not work.')
 
-while not exists(TOKEN_PATH):
+while not exists(token_path):
   sleep(2)
 
-if isfile(TOKEN_PATH):
-  token = open(TOKEN_PATH, 'r').read().strip()
-  addK3SToken(token)
+if isfile(token_path, var):
+  var_val = open(token_path, 'r').read().strip()
+  updateOrAddAppVar(app_id, token, var, var_val)
 else:
   print('I done effed up, Sarge.')
