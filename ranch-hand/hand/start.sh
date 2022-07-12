99620866d81e878314f0f64700abbd37e02a112f
SamEureka <sameureka@me.com>
2022-07-12 07:43:51 -0400

    stuff and things were completed

--- ranch-hand/hand/start.sh
+++ ranch-hand/hand/start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+## balenaRancher/ranch-hand-amd64/start.sh
+## Sam Dennon//2022
+
+if [[ -z "$DEVICE_HOSTNAME" ]]; then
+  DEVICE_HOSTNAME=$BALENA_DEVICE_NAME_AT_INIT
+  HOSTNAME=$DEVICE_HOSTNAME
+fi
+
+curl -s -X PATCH --header "Content-Type:application/json" \
+  --data '{"network": {"hostname": "'"${DEVICE_HOSTNAME}"'"}}' \
+  "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" >/dev/null
+
 echo "Starting k3s on ${HOSTNAME}, connecting to ${K3S_URL}"
 /opt/k3s/k3s-arm64 agent
